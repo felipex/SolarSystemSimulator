a2662b748ca859c410879db03177793640413fb9
Felipe Cavalcante <felipexcavalcante@gmail.com>
2025-02-06 12:36:14 +0000

    Inicial

--- app.py
+++ app.py
@@ -7,7 +7,32 @@ logging.basicConfig(level=logging.DEBUG)
 # Create Flask app
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """Render the main page with the solar system visualization."""
     return render_template('index.html')
+
+
+@app.route('/0')
+def index0():
+    """Render the main page with the solar system visualization."""
+    return render_template('index0.html')
+
+
+@app.route('/2')
+def index2():
+    """Render the main page with the solar system visualization."""
+    return render_template('index2.html')
+
+
+@app.route('/3')
+def index3():
+    """Render the main page with the solar system visualization."""
+    return render_template('index3.html')
+
+
+@app.route('/10')
+def index10():
+    """Render the main page with the solar system visualization."""
+    return render_template('index10.html')
